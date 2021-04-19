9e8aef748eb3ef3eaa9f0171b3c88b436fb8653f
Henry Koleoso <Henry_Koleoso@hotmail.co.uk>
2021-04-19 13:37:34 +0100

    add basic character display

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-def add(n):
-    print(int(n) + int(n))
+import pygame
+import os
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Cat Adventure")
+x = 200
+y = 200
+
+img = pygame.image.load('img/cat/idle/mario.png')
+rect = img.get_rect()
+rect.centre = (x, y)
 
 
-add(input("Numbers to double "))
+running = True
+
+while running:
+
+    screen.blit(img, rect)
+
+    for event in pygame.event.get():
+        # quits the game
+        if event.type == pygame.QUIT:
+            running = False
+
+        pygame.display.update()
+
+
+pygame.quit()
