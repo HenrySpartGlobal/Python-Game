809a74ae23461d716a5c8f26edab47a211651e10
henrykoleoso <henry_koleoso@hotmail.co.uk>
2021-04-20 23:39:34 +0100

    add full movement animations for character

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -6,123 +7,171 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Cat Adventure")
+pygame.display.set_caption('Rells Adventure')
 
-# frame rate
+# set framerate
 clock = pygame.time.Clock()
-fps = 60
+FPS = 60
 
-# player action variables
+# define game variables
+GRAVITY = 0.75
+
+# define Rell action variables
 moving_left = False
 moving_right = False
 
-# colours
+# define colours
 BG = (144, 201, 120)
+RED = (255, 0, 0)
 
 
 def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 
-class Cat(pygame.sprite.Sprite):
+class Rell(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
         self.flip = False
         self.animation_list = []
         self.frame_index = 0
+        self.action = 0
         self.update_time = pygame.time.get_ticks()
-        for i in range(18):
-            img = pygame.image.load(f'img/{self.char_type}/idle/{i}.png')
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            self.animation_list.append(img)
-        self.image = self.animation_list[self.frame_index]
+
+        # load all images for the players
+        animation_types = ['Idle', 'Run', 'Jump']
+        for animation in animation_types:
+            # reset temporary list of images
+            temp_list = []
+            # count number of files in the folder
+            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
-    def move(self, left, right):
-        # movement vars delta
+    def move(self, moving_left, moving_right):
+        # reset movement variables
         dx = 0
         dy = 0
 
-        # movement var for left or right
-        if left:
+        # assign movement variables if moving left or right
+        if moving_left:
             dx = -self.speed
             self.flip = True
             self.direction = -1
-        if right:
+        if moving_right:
             dx = self.speed
             self.flip = False
             self.direction = 1
 
-        # move rect pos
+        # jump
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        # apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        # check collision with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+
+        # update rectangle position
         self.rect.x += dx
         self.rect.y += dy
 
-    def update_anim(self):
-        # update time
+    def update_animation(self):
+        # update animation
         ANIMATION_COOLDOWN = 100
-        # update image depending on frame
-        self.image = self.animation_list[self.frame_index]
-        # check when last update was
+        # update image depending on current frame
+        self.image = self.animation_list[self.action][self.frame_index]
+        # check if enough time has passed since the last update
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        # loop animation back to start
-            if self.frame_index >= len(self.animation_list):
-                self.frame_index = 0
+        # if the animation has run out the reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        # check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
-player = Cat('rell', 200, 200, 2, 5)
+player = Rell('Rell', 200, 200, 3, 5)
+enemy = Rell('enemy', 400, 200, 3, 5)
+cat = Rell('cat', 400, 200, 3, 5)
 
-# todo change animation loop for cat
-enemy = Cat('rell', 300, 200, 2, 5)
-running = True
-new_time, old_time = None, None
+run = True
+while run:
 
-while running:
+    clock.tick(FPS)
 
-    clock.tick(fps)
     draw_bg()
 
-    player.update_anim()
+    player.update_animation()
     player.draw()
+    cat.draw()
     enemy.draw()
 
-    player.move(moving_left, moving_right)
+    # update Rell actions
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)  # 2: jump
+        elif moving_left or moving_right:
+            player.update_action(1)  # 1: run
+        else:
+            player.update_action(0)  # 0: idle
+        player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
-        # quits the game
+        # quit game
         if event.type == pygame.QUIT:
-            running = False
-        # controls
+            run = False
+        # keyboard presses
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
-                running = False
+                run = False
 
-        # button let go
+        # keyboard button released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
 
-    # display fps and milliseconds
-    if new_time:
-        old_time = new_time
-    new_time = pygame.time.get_ticks()
-    if new_time and old_time:
-        pygame.display.set_caption("FPS " + (str(int(clock.get_fps())) + " ms: " + str(new_time - old_time)))
-
-        pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
