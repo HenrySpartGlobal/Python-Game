2344b0c458fd8824bb3e3c1b8abbee9e70dd333f
henrykoleoso <henry_koleoso@hotmail.co.uk>
2021-04-20 09:17:53 +0100

    add player movement

--- main.py
+++ main.py
@@ -7,28 +7,92 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Cat Adventure")
-x = 200
-y = 200
-scale = 0.3
 
-img = pygame.image.load('img/cat/idle/mario.png')
-img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-rect = img.get_rect()
-rect.center = (x, y)
+# frame rate
+clock = pygame.time.Clock()
+fps = 60
+
+# player action variables
+moving_left = False
+moving_right = False
+
+# colours
+BG = (144, 201, 120)
 
 
+def draw_bg():
+    screen.fill(BG)
+
+
+class Cat(pygame.sprite.Sprite):
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load(f'img/{self.char_type}/idle/mario.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, left, right):
+        # movement vars delta
+        dx = 0
+        dy = 0
+
+        # movement var for left or right
+        if left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        # move rect pos
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+player = Cat('cat', 200, 200, 0.1, 5)
+enemy = Cat('cat', 300, 200, 0.1, 5)
 running = True
 
 while running:
 
-    screen.blit(img, rect)
+    clock.tick(fps)
+    draw_bg()
+
+    player.draw()
+    enemy.draw()
+
+    player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         # quits the game
         if event.type == pygame.QUIT:
             running = False
+        # controls
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+        # button let go
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
 
         pygame.display.update()
-
 
 pygame.quit()
